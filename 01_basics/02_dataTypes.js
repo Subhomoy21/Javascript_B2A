5538eb44aa127b79c43ee9125cdadf06657f5013
Subhomoy Das <104619658+Subhomoy21@users.noreply.github.com>
2024-03-01 19:07:59 +0000

    EMCA standards and datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+// admit it that js is changed a lot like your gf!
+"use strict" ; // treat all js code as newer version
+// alert("hello") we are using nodejs not browser
+
+console.log(3 + 3); console.log("Subhomoy") // work toh karra but readable nhi hai!
+// run hoga 
+// so remember that code readability should be high
+//**   / ecmaScript ke uper research karo!
+
+let name = "Subhomoy" // string datatype
+let age = 21 // integer
+let isLoggedIn = false //boolean
+let state = null // empty 
+
+// numbers => 2^53
+// bigInteger(yaad kar java mein parra tha used in trading for playing with 
+// large data) =>
+// string => ""
+// boolean => true/false 
+// null(stand alone value in js)
+// undefined => when the value is not assigned
+
